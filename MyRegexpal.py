56a1f28eb5d715f4f70e0b52127560a30027e770
Junior <jguevarat@uni.pe>
2020-11-14 13:47:52 -0500

    Show macthes

--- MyRegexpal.py
+++ MyRegexpal.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import scrolledtext as st
+import re
 
 
 class Aplicacion(Frame):
@@ -23,23 +24,39 @@ class Aplicacion(Frame):
 
         self.label1 = ttk.Label(self.cont1, text="Expresion Regular")
         self.re = StringVar()
-        self.txt1 = ttk.Entry(self.cont1, textvariable=self.re, width=75)
+        self.txt1 = ttk.Entry(self.cont1, textvariable=self.re, width=80)
         self.label1.grid(row=0, column=0, sticky=(N, W, E, S), padx=5, pady=5)
         self.txt1.grid(row=1, column=0, sticky=(N, W, E, S), padx=5, pady=5)
-        self.bt1 = ttk.Button(self.cont1, text="Buscar", command=self.uno)
+        self.bt1 = ttk.Button(self.cont1, text="Buscar", command=self.buscar)
         self.bt1.grid(row=1, column=1, sticky=(N, W, E, S), padx=5, pady=5)
 
         self.label2 = ttk.Label(self.cont2, text="Texto")
-        self.texto = StringVar()
-        self.txt2 = st.ScrolledText(self.cont2, width=70, height=10)
+        self.txt2 = st.ScrolledText(self.cont2, width=70, height=6)
         self.label2.grid(row=0, column=0, sticky=(N, W, E, S), padx=5, pady=5)
         self.txt2.grid(row=1, column=0, sticky=(N, W, E, S), padx=5, pady=5)
 
-        self.lista1 = ttk.Treeview(self.cont3)
-        self.lista1.grid(row=0, column=0, sticky=(W), padx=5, pady=5)
+        self.lista1 = ttk.Treeview(self.cont3, height=5)
+        self.lista1.heading('#0', text="Matches")
+        self.lista1.column("#0", width=568, minwidth=270, stretch=NO)
+        self.lista1.grid(row=0, column=0, sticky=(N, W, E, S), padx=0, pady=5)
+        self.vsb = ttk.Scrollbar(self.cont3, orient="vertical",
+                                 command=self.lista1.yview)
+        self.vsb.grid(row=0, column=1, sticky=(N, W, E, S), pady=5)
+        self.lista1.configure(yscrollcommand=self.vsb.set)
+        self.Label3Matches = StringVar(value="Numero de matches: 0")
+        self.label3 = ttk.Label(self.cont3, textvariable=self.Label3Matches)
+        self.label3.grid(row=1, column=0, sticky=(N, W, E, S), padx=5, pady=5)
 
-    def uno(self):
-        pass
+    def buscar(self):
+        self.lista1.delete(*self.lista1.get_children())
+
+        self.compilado = re.compile(self.re.get())
+        listaMacth = self.compilado.findall(self.txt2.get("1.0", END))
+
+        for m in listaMacth:
+            self.lista1.insert('', END, text=m)
+
+        self.Label3Matches.set('Numero de matches: {}'.format(len(listaMacth)))
 
 
 if __name__ == "__main__":
