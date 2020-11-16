f6beefcbf61e3e3d7bf1d8d7bd82615c81214af7
Junior <jguevarat@uni.pe>
2020-11-16 16:07:19 -0500

    Add feature replace

--- MyRegexpal.py
+++ MyRegexpal.py
@@ -20,10 +20,12 @@ class Aplicacion(Frame):
         self.cont2 = ttk.Frame(self.mainFrame, padding="10 10 10 10")
         self.cont3 = ttk.Frame(self.mainFrame, padding="10 10 10 10")
         self.cont4 = ttk.Frame(self.mainFrame, padding="10 10 10 10")
+        self.cont5 = ttk.Frame(self.mainFrame, padding="10 10 10 10")
         self.cont1.grid(row=0, column=0, sticky=(N, W, E, S))
         self.cont2.grid(row=1, column=0, sticky=(N, W, E, S))
         self.cont3.grid(row=2, column=0, sticky=(N, W, E, S))
         self.cont4.grid(row=3, column=0, sticky=(N, W, E, S))
+        self.cont5.grid(row=4, column=0, sticky=(N, W, E, S))
 
         self.label1 = ttk.Label(self.cont1, text="Expresion Regular")
         self.re = StringVar()
@@ -59,6 +61,24 @@ class Aplicacion(Frame):
         self.vsb2.grid(row=0, column=1, sticky=(N, W, E, S), pady=5)
         self.lista2.configure(yscrollcommand=self.vsb2.set)
 
+        self.label4 = ttk.Label(self.cont5, text='Grupo (entero ejem: 1)')
+        self.label4.grid(row=0, column=0, sticky=(N, W, E, S), padx=5, pady=5)
+        self.label5 = ttk.Label(self.cont5, text='Reemplazo')
+        self.label5.grid(row=0, column=1, sticky=(N, W, E, S), padx=5, pady=5)
+        self.grupoReplace = StringVar()
+        self.txt3 = ttk.Entry(self.cont5,
+                              textvariable=self.grupoReplace, width=30)
+        self.txt3.grid(row=1, column=0, sticky=(N, W, E, S), padx=5, pady=5)
+        self.reemplazo = StringVar()
+        self.txt4 = ttk.Entry(self.cont5,
+                              textvariable=self.reemplazo, width=30)
+        self.txt4.grid(row=1, column=1, sticky=(N, W, E, S), padx=5, pady=5)
+        self.bt2 = ttk.Button(self.cont5, text="Replace Group",
+                              command=self.replaceGroup)
+        self.bt2.grid(row=1, column=2, sticky=(N, W, E, S), padx=5, pady=5)
+        self.txt3 = st.ScrolledText(self.cont5, width=70, height=5)
+        self.txt3.grid(row=2, column=0, columnspan=4, sticky=(N, W, E, S), padx=5, pady=5)
+
     def buscar(self):
         self.lista1.delete(*self.lista1.get_children())
         self.lista2.delete(*self.lista2.get_children())
@@ -83,6 +103,31 @@ class Aplicacion(Frame):
                     self.lista2.insert('', END, text=g)
                 nmatches = nmatches + 1
                 self.Label3Matches.set('Total de matches: {}'.format(nmatches))
+        except re.error as msg:
+            messagebox.showerror("Error", msg)
+
+    def replaceGroup(self):
+        texto = self.txt2.get("1.0", END)
+        nuevotexto = texto[:]
+        gr = self.grupoReplace.get()  # Grupoa reemplazar
+        newcd = self.reemplazo.get()  # Cadena que pondra
+        self.txt3.delete(1.0, END)
+        try:
+            self.compilado = re.compile(self.re.get(), re.MULTILINE)
+            last = 0
+            while last <= len(texto):
+                m = self.compilado.search(texto, last)
+                if m is None:
+                    break
+                first, last = m.span()
+                if last == first:
+                    last = first+1
+                groups = list(m.groups())
+                groups.insert(0, m.group())
+                new = re.sub(groups[int(gr)], newcd, groups[0])
+                nuevotexto = nuevotexto.replace(nuevotexto[first:last], new)
+                self.txt3.delete(1.0, END)
+                self.txt3.insert(END, nuevotexto)
         except re.error as msg:
             messagebox.showerror("Error", msg)
 
